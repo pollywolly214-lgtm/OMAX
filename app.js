bb1a5e980337cf6c08a4b8a39c1dfaa99e1deacc
pollywolly214-lgtm <pollywolly214@gmail.com>
2025-09-05 10:57:21 -0500

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const btn = document.getElementById("helloBtn");
-  const out = document.getElementById("output");
-
-  btn.addEventListener("click", () => {
-    out.textContent = "JavaScript is working 🎉 Your site is live!";
-  });
-});
